0d8584038eb3c32d2f7f900c44b7aacf12655a3f
Andrei Scripniciuc <scripni@users.noreply.github.com>
2016-02-15 18:24:52 +0000

    added basic binding

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,29 @@
 'use strict';
+
+angular.module('shoppingListApp', [])
+  .controller('homeCtrl',
+    function homeCtrl($scope) {
+      $scope.list = {
+        items: [
+          {
+            name: "milk",
+            quantity: 2
+          },
+          {
+            name: "bread",
+            quantity: 4
+          }
+        ]
+      };
+
+      $scope.addToList = function() {
+        console.log($scope.toAdd);
+        $scope.list.items.push({
+          name: $scope.toAdd,
+          quantity: 1
+        });
+
+        $scope.toAdd = '';
+      };
+    }
+  );
